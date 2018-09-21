af9d672a38466c3a846ee8587d9008505c883b49
Arsalan Sadri <asadri62@gmail.com>
2018-09-21 01:32:25 -0700

    >> added package.json, sql schema and seeds <<

---
+++ seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Round dining table", "Furniture", 649.99, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Coffee table", "Furniture", 44.99, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Leather sofa", "Furniture", 549.99, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("MacBook Pro", "Electronics", 3200.00, 35);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPhone X", "Electronics", 1200.00, 70);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sony 4K 55inch TV", "Electronics", 2500, 24);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Slim-talored jeans", "Clothing", 119.99, 38);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Skinny jeans", "Clothing", 109.99, 45);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Carry-on suitcase" , "Luggage", 89.99, 13);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Large spinner suitcase", "Luggage", 249.99, 19);
