c7d772021e38bbb1a22cc8e0cb935b4fdcb8cae6
Arsalan Sadri <asadri62@gmail.com>
2018-09-24 01:09:33 -0700

    some file renaming

--- seeds.sql
+++ seeds.sql
@@ -1,11 +1,11 @@
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Round dining table", "Furniture", 649.99, 25);
+VALUES ("dining table", "Furniture", 649.99, 25);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Coffee table", "Furniture", 44.99, 60);
+VALUES ("coffee table", "Furniture", 44.99, 60);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Leather sofa", "Furniture", 549.99, 20);
+VALUES ("leather sofa", "Furniture", 549.99, 20);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("MacBook Pro", "Electronics", 3200.00, 35);
@@ -14,16 +14,16 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("iPhone X", "Electronics", 1200.00, 70);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Sony 4K 55inch TV", "Electronics", 2500, 24);
+VALUES ("sony 4K 55inch TV", "Electronics", 2500, 24);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Slim-talored jeans", "Clothing", 119.99, 38);
+VALUES ("slim jeans", "Clothing", 119.99, 38);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Skinny jeans", "Clothing", 109.99, 45);
+VALUES ("skinny jeans", "Clothing", 109.99, 45);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Carry-on suitcase" , "Luggage", 89.99, 13);
+VALUES ("carry-on suitcase" , "Luggage", 89.99, 13);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Large spinner suitcase", "Luggage", 249.99, 19);
+VALUES ("large spinner suitcase", "Luggage", 249.99, 19);
